3bf2822fbc8e3cc6f9155463812b7629e0d1656f
sarat <sarat@DESKTOP-QCP8047>
2026-06-12 02:38:06 +0400

    Добавьте файлы проекта.

---
+++ HandToHand/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HandToHand
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+        string userText = UserInput.Text.Trim();
+            if (string.IsNullOrEmpty(userText))
+            {
+                return;
+            }
+        ChatLog.Text += "\n\nВы: " + userText;
+        UserInput.Text = "";
+
+        }
+    }
+}
